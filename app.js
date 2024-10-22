43d2b80ac3346e7773344d4979d41546393f0e0a
Kalyani Garkade <77484202+guddi-al@users.noreply.github.com>
2024-10-22 16:20:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+        .controller('ShoppingListController', ShoppingListController);
+
+    ShoppingListController.$inject = [];
+
+    function ShoppingListController() {
+        var list = this;
+
+        list.toBuy = [
+            { name: "cookies", quantity: 10 },
+            { name: "chips", quantity: 5 },
+            { name: "soda", quantity: 3 },
+            { name: "candy", quantity: 4 },
+            { name: "milk", quantity: 2 }
+        ];
+
+        list.alreadyBought = [];
+
+        list.buyItem = function (index) {
+            var item = list.toBuy.splice(index, 1)[0];
+            list.alreadyBought.push(item);
+        };
+    }
+})();
